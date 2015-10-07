e1e20da3ffaa69f34bce7cc2d5cac080b58774d3
hnam2812 <hnam2812@gmail.com>
2015-10-07 15:35:49 +0700

    Finished update,show without delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
+    def index
+        @users = User.paginate(page: params[:page])
+    end
   def new
   	 @user = User.new
   end
@@ -14,6 +19,33 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+def edit
+    @user = User.find(params[:id])
+  end
+
+ def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+     flash[:success] = "Profile updated"
+      redirect_to @user
+
+    else
+      render 'edit'
+    end
+  end
+  def logged_in_user
+    unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+
+  end
+  def correct_user
+     @user = User.find(params[:id])
+     redirect_to(root_url) unless current_user?(@user)
+
   end
 
   private
