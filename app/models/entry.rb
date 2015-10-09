76c60c29c07f4de6894a70c907fbca0941c85cd5
hnam2812 <hnam2812@gmail.com>
2015-10-09 15:15:03 +0700

    Entry'content can now >3000 char

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,7 @@ class Entry < ActiveRecord::Base
   has_many :comment, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 300 }
+  validates :content, presence: true, length: { maximum: 2000 }
   validates :title, presence: true, length: {maximum: 140}
 
 end
