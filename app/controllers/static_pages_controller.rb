8c63716ceda9277adcc65af913050821ad41f336
hnam2812 <hnam2812@gmail.com>
2015-10-15 08:29:50 +0700

    Change static page view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   	 @entry = current_user.entry.build if logged_in?
-  	 @entries = current_user.show_entries.paginate(page: params[:page]) if logged_in?
+  	 @entries = current_user.show_entries.paginate(page: params[:page],per_page: 4) if logged_in?
   end
 
   def about
